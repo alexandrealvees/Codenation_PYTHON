9802384743ae3236376ac654672b8ede898a3ec1
Alexandre Alves <alexandrealveees@outlook.com>
2020-05-25 22:57:22 -0300

    Add files via upload

--- Desafio 3/main.py
+++ Desafio 3/main.py
@@ -2,7 +2,7 @@ import requests
 
 
 def get_temperature(lat, lng):
-    key = '38853fa7bad13c5f14b1752b4974837585a48a34'
+    key = 'e1ee55658d4a2b28c4841e373c3b3d87'
     url = 'https://api.darksky.net/forecast/{}/{},{}'.format(key, lat, lng)
     reponse = requests.get(url)
     data = reponse.json()
